0e20e93e6695b7856ffba2fc205d33c99cf43cbc
Rajitha Ramanayake <rajitharamanayake@gmail.com>
2015-09-23 22:20:53 +0530

    sdatabase class ok

--- src/DBStocks.java
+++ src/DBStocks.java
@@ -6,10 +6,10 @@ import java.io.*;
 import java.util.*;
 
 public class DBStocks {
-    private Map<String, String> stockList;
+    private Map<String, String[]> stockList;
     private String [] fields;
 
-    public DBStocks(String cvsFile, String key, String value) {
+    public DBStocks(String cvsFile, String key, String value1,String value2) {
         FileReader fileRd=null;
         BufferedReader reader=null;
 
@@ -25,24 +25,28 @@ public class DBStocks {
 
             // find where the key and the value are
             int keyIndex = findIndex(key);
-            int valIndex = findIndex(value);
+            int val1Index = findIndex(value1);
+            int val2Index = findIndex(value2);
 
-            if(keyIndex == -1 || valIndex == -1)
+            if(keyIndex == -1 || val1Index == -1 || val2Index == -1)
                 throw new IOException("CVS file does not have data");
             // note how you can throw a new exception
 
             // get a new hash map
-            stockList = new HashMap<String, String>();
+            stockList = new HashMap<String, String[]>();
 
 	    /* read each line, getting it split by ,
 	     * use the indexes to get the key and value
 	     */
             String [] tokens;
+            String [] tokens2 = new String[2];
             for(String line = reader.readLine();
                 line != null;
                 line = reader.readLine()) {
                 tokens = line.split(",");
-                stockList.put(tokens[keyIndex], tokens[valIndex]);
+                tokens2[0] = tokens[val1Index];
+                tokens2[1] = tokens[val2Index];
+                stockList.put(tokens[keyIndex], tokens2);
             }
 
             if(fileRd != null) fileRd.close();
@@ -65,6 +69,7 @@ public class DBStocks {
     }
 
     public String findName(String key) {
-        return stockList.get(key);
+        String[] arr = stockList.get(key);
+        return arr[0];
     }
 }
