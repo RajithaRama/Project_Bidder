df4f2c964a1a92b335549911e391c41a1b0813ce
Rajitha Ramanayake <rajitharamanayake@gmail.com>
2015-09-23 22:51:50 +0530

    Display created

---
+++ src/Display.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Rama on 9/23/2015.
+ */
+public class Display extends JFrame implements ActionListener{
+
+}
--- src/MainServer.java
+++ src/MainServer.java
@@ -2,4 +2,5 @@
  * Created by Rama on 9/23/2015.
  */
 public class MainServer {
+
 }
