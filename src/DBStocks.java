f3643d01f3ca3aa1e41f9041d9ae4277b7b4ebaa
Rajitha Ramanayake <rajitharamanayake@gmail.com>
2015-09-24 17:23:56 +0530

    DBStock Changed

--- src/DBStocks.java
+++ src/DBStocks.java
@@ -2,11 +2,13 @@
  * Created by Rama on 9/23/2015.
  */
 
+import oracle.jrockit.jfr.StringConstantPool;
+
 import java.io.*;
 import java.util.*;
 
 public class DBStocks {
-    private Map<String, String[]> stockList;
+    private Map<String, Tuple> stockList;
     private String [] fields;
 
     public DBStocks(String cvsFile, String key, String value1,String value2) {
@@ -33,19 +35,18 @@ public class DBStocks {
             // note how you can throw a new exception
 
             // get a new hash map
-            stockList = new HashMap<String, String[]>();
+            stockList = new HashMap<String, Tuple>();
 
 	    /* read each line, getting it split by ,
 	     * use the indexes to get the key and value
 	     */
             String [] tokens;
-            String [] tokens2 = new String[2];
+            Tuple<String, Integer> tokens2;
             for(String line = reader.readLine();
                 line != null;
                 line = reader.readLine()) {
                 tokens = line.split(",");
-                tokens2[0] = tokens[val1Index];
-                tokens2[1] = tokens[val2Index];
+                tokens2 = new Tuple(tokens[val1Index],tokens[val2Index]);
                 stockList.put(tokens[keyIndex], tokens2);
             }
 
@@ -69,7 +70,12 @@ public class DBStocks {
     }
 
     public String findName(String key) {
-        String[] arr = stockList.get(key);
-        return arr[0];
+        Tuple<String,Integer> arr = stockList.get(key);
+        return arr.x;
+    }
+
+    public int findPrice(String key) {
+        Tuple<String,Integer> arr = stockList.get(key);
+        return arr.y;
     }
 }
--- src/Display.java
+++ src/Display.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
@@ -6,4 +7,33 @@ import java.awt.event.ActionListener;
  */
 public class Display extends JFrame implements ActionListener{
 
+    private JPanel server;
+    private JLabel stockFB = new JLabel("FB");
+    private JLabel stockVRTU = new JLabel("VRTU");
+    private JLabel stockMSFT = new JLabel("MSFT");
+    private JLabel stockGOOGL = new JLabel("GOOGL");
+    private JLabel stockYHOO = new JLabel("YHOO");
+    private JLabel stockXLNX = new JLabel("XLNX");
+    private JLabel stockTSLA = new JLabel("TSLA");
+    private JLabel stockTXN = new JLabel("TXN");
+
+
+
+
+
+    public Display(){
+
+        super("Stock Price");
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Timer timer = new Timer(500, this);
+        timer.start();
+
+
+    }
+
+
+    public void actionPerformed(ActionEvent e){
+
+    }
 }
